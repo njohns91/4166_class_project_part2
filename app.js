70e032d14f446d8e37956f0668a2bdc8ee44b902
njohns91 <97905712+njohns91@users.noreply.github.com>
2023-02-22 17:16:29 -0500

    update
    initial start

---
+++ app.js
@@ -0,0 +1,26 @@
+//requre modules
+const express = require('express');
+const morgan = require('morgan');
+
+//create application
+const app = express();
+
+//configure application
+let port = 3000;
+let host = 'localhost';
+app.set('view engine', 'ejs');
+
+//mount middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('tiny'));
+
+//set up routes
+app.get('/', (req, res)=>{
+    res.render('index');
+});
+
+//start the server
+app.listen(port, host, () =>{
+    console.log('Server is runnint on port', port);
+});
