be13cc99a9a70de85f1f9c82b26b9a23c803ca46
njohns91 <97905712+njohns91@users.noreply.github.com>
2023-02-23 02:26:28 -0500

    update

--- models/event.js
+++ models/event.js
@@ -4,18 +4,14 @@ const events = [
     {
         id: '1',
         title: 'Hay Bail Ride',
-        content1: 'Red Tractor (Short Path)',
-        content2: 'Blue Tractor (Medium path)',
-        content3: 'Green Tractor (Long Path)',
+        content: 'Red Tractor (Short Path), '+ 'Blue Tractor (Medium path), '+'Green Tractor (Long Path)',
         author: 'Nick Johnson',
         createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
     },
     {
         id: '2',
         title: 'Corn Maze',
-        content1: 'Beginner (All Ages)',
-        content2: 'Intermediate (6 and up)',
-        content3: 'Expert (12 and up)',
+        content: 'Beginner (All Ages)' + 'Intermediate (6 and up)'+ 'Expert (12 and up)',
         author: 'Nick Johnson',
         createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
     }
